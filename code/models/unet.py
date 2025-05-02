27654581039e0ea4471dc69549d165b7ff78062b
citade1 <86925181+citade1@users.noreply.github.com>
2025-05-02 17:21:19 +0900

    Update unet.py

--- code/models/unet.py
+++ code/models/unet.py
@@ -6,56 +6,63 @@ class UNet(nn.Module):
     def __init__(self, in_channels=1, out_channels=1, features=[64,128,256]):
         super(UNet, self).__init__()
 
-        # Encoder
-        self.downs = nn.ModuleList()
-        for feature in features:
-            self.downs.append(self._block(in_channels, feature))
-            in_channels = feature
-        
-        # Bottleneck
-        self.bottleneck = self._block(features[-1], features[-1]*2)
+        self.encoder_blocks = nn.ModuleList()
+        self.downsamples = nn.ModuleList()
 
-        # Decoder
-        self.ups = nn.ModuleList()
-        for feature in reversed(features):
-            self.ups.append(
-                nn.ConvTranspose2d(feature*2, feature, kernel_size=2, stride=2) # this convtranspose doubles spatial size
-            )
-            self.ups.append(self._block(feature*2, feature))
+        # Encoder: Conv -> BN -> ReLU -> Strided Conv for downsampling
+        for feature in features:
+            self.encoder_blocks.append(self._conv_block(in_channels, feature))
+            self.downsamples.append(nn.Conv2d(feature, feature, kernel_size=2, stride=2))
+            in_channels=feature
+
+        # Bottleneck
+        self.bottleneck = self._conv_block(features[-1], features[-1]*2)
+
+        # Decoder: Upsample -> Concat(w/ skip connection) -> Conv(->BN ->ReLU)
+        self.upsamples = nn.ModuleList()
+        self.decoder_blocks = nn.ModuleList()
+        reversed_features = features[::-1]
+        for feature in reversed_features:
+            self.upsamples.append(nn.ConvTranspose2d(feature*2, feature, kernel_size=2, stride=2))
+            self.decoder_blocks.append(self._conv_block(feature*2, feature))
         
-        # Final output layer
+        # Final 1x1 conv for mask output(0~1)
         self.final_conv = nn.Conv2d(features[0], out_channels, kernel_size=1)
+        self.activation = nn.Sigmoid()
     
-    # Input -> x:(B, 1, F, T) = (batch_size, channels=1, frequency_bins, time_frames)
-    def forward(self, x): 
-        
+
+    def forward(self, x):
         skip_connections = []
 
-        # Downsampling
-        for down in self.downs:
-            x = down(x)
+        # Encoder
+        for encoder, downsample in zip(self.encoder_blocks, self.downsamples):
+            x = encoder(x)
             skip_connections.append(x)
-            x = F.max_pool2d(x, kernel_size=2)
+            x = downsample(x) 
         
-        # Bottleneck
-        x = self.bottlenect(x)
+        # Bottleneck 
+        x = self.bottleneck(x)
         skip_connections = skip_connections[::-1]
 
-        # Upsampling
-        for i in range(0, len(self.ups), 2):
-            x = self.ups[i](x) # ConvTranspose2d 
-            skip_connections = skip_connections[i//2]
-            if x.shape != skip_connections.shape:
-                x = F.interpolate(x, size=skip_connections.shape[2:]) # Match size
-            x = torch.cat((skip_connections, x), dim=1) # channel-wise concat
-            x = self.ups[i+1](x) # Conv block
+        # Decoder
+        for i in range(len(self.upsamples)):
+            x = self.upsamples[i](x)
+            skip = skip_connections[i]
+            if x.shape != skip.shape:
+                x = F.interpolate(x, size=skip.shape[2:])
+            x = torch.cat((skip, x), dim=1)
+            x = self.decoder_blocks[i](x)
         
-        return self.final_conv(x)
-    
-    def _block(self, in_channels, out_channels):
+        x = self.final_conv(x)
+        return self.activation(x)
+
+
+    def _conv_block(self, in_channels, out_channels):
         return nn.Sequential(
             nn.Conv2d(in_channels, out_channels, 3, padding=1),
+            nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
             nn.Conv2d(out_channels, out_channels, 3, padding=1),
-            nn.ReLU(replace=True)
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
         )
